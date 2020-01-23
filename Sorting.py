e84f3c959409fd8b3bbb290e3f24ba21a7b5b1c0
Lohit-Mohapatra <54813043+Lohit-Mohapatra@users.noreply.github.com>
2020-01-23 15:17:08 +0530

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -19,3 +19,43 @@ def insertionSort(nlist):
 nlist = [14,46,43,27,57,41,45,21,70]
 insertionSort(nlist)
 print(nlist)
+
+
+Selection Sort
+--------------
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning
+
+arr = [10,9,7,6,5,4,12,32,43,29]
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min_idx=i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx=j
+                
+        arr[min_idx], arr[i] = arr[i], arr[min_idx]  
+        
+selectionSort(arr)
+print(arr)
+
+
+Bubble Sort
+-----------
+Bubble Sort works by repeatedly swapping the adjacent elements if they are in wrong order.
+
+arr = [10,9,7,6,5,4,12,32,43,29]
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+               arr[j], arr[j+1] = arr[j+1], arr[j]
+            
+bubbleSort(arr)
+print(arr)    
+
+
+
+Difference between BubbleSort, InsertionSort and SelectionSort :
+Bubble sort repeatedly compares and swaps(if needed) adjacent elements in every pass. 
+Selection sort takes the current element and exchange it with the smallest element on the right hand side of the current element.
+Insertion sort take the current element and insert it at the appropriate position of the list, adjusting the list every time you insert. It is similar to arranging the cards in a Card game.
